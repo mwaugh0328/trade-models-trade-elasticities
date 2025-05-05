a054377542c669a2ebe83448dd72a6da06d182dc
mikew <waugh.e.michael@gmail.com>
2025-05-05 11:32:12 -0400

    price gap .jl runs

--- build-data-code/make_price_data.jl
+++ build-data-code/make_price_data.jl
@@ -6,20 +6,20 @@ using MAT
 include("functions_make_trade_share.jl")
 ###########################################################################################
 
-input_isic = readdlm("./make-tradeshare-pricegap/trade-data/isic_3digit_manuf.txt")
+input_isic = readdlm("./build-data-code/feenstra-trade-data/isic_3digit_manuf.txt")
 
-drop_30 = DataFrame(CSV.File("./make-tradeshare-pricegap/drop_30.csv"))
+drop_30 = DataFrame(CSV.File("./build-data-code/drop_30.csv"))
 
 ###########################################################################################
 
-data_set = XLSX.readxlsx("./make-tradeshare-pricegap/price-data/clean_basic_headings_2011.xlsx")
+data_set = XLSX.readxlsx("./build-data-code/2011-price-data/clean_basic_headings_2011.xlsx")
 
 # Extract the sheet (replace "Sheet1" with the actual sheet name if needed)
 price_data = data_set["Sheet1"][1:136,1:160]
 
 pricemat = adjust_price_data(price_data)
 
-trade_flow_file = "./make-tradeshare-pricegap/trade-data/wtf_11mat.txt"
+trade_flow_file = "./build-data-code/feenstra-trade-data/wtf_11mat.txt"
 
 input_wtf = readdlm(trade_flow_file, ',', Float64)
 
@@ -30,53 +30,53 @@ trademat = construct_tradematrix(input_wtf, input_isic)
 new_trade_mat = aggregate_drop(trademat, drop_30[:, "new top 30"])
 
 # grab the gross output data
-gross_output_file = "./make-tradeshare-pricegap/UNIDO-data/UNIDO-grossoutput-data-2011.csv"
+gross_output_file = "./build-data-code/UNIDO-data/UNIDO-grossoutput-data-2011.csv"
 
 gross_output = DataFrame(CSV.File(gross_output_file))
 
 tradeshare, tradeshare_df = construct_tradeshare(new_trade_mat, gross_output)
 
-# df = build_dni(pricemat, gross_output, tradeshare)
+df = build_dni(pricemat, gross_output, tradeshare)
 
-# outfile = "pricegap-df-2011.csv"
+outfile = "pricegap-df-2011.csv"
 
-# CSV.write(outfile, df, header = true)  # Write the DataFrame to a CSV file
+CSV.write(outfile, df, header = true)  # Write the DataFrame to a CSV file
 
-###########################################################################################
-###########################################################################################
+##########################################################################################
+##########################################################################################
 
-# mat_data = matread("./old-code/estimation_mat_30_2014-version.mat")
+mat_data = matread("./build-data-code/2004-price-data/estimation_mat_30_2014-version.mat")
 
-# pricemat = mat_data["pmat_30"]
+pricemat = mat_data["pmat_30"]
 
-# istraded = mat_data["istraded"]
+istraded = mat_data["istraded"]
 
-# ttt = istraded .≈ 0.0
+ttt = istraded .≈ 0.0
 
-# pricemat = pricemat[:, .!vec(ttt)]
+pricemat = pricemat[:, .!vec(ttt)]
 
-# trade_flow_file = "./make-tradeshare-pricegap/trade-data/wtf_04mat.txt"
+trade_flow_file = "./build-data-code/feenstra-trade-data/wtf_04mat.txt"
 
-# input_wtf = readdlm(trade_flow_file, ',', Float64)
+input_wtf = readdlm(trade_flow_file, ',', Float64)
 
-# # Construct the aggregate bilateral trade flow matrix
+# Construct the aggregate bilateral trade flow matrix
 
-# trademat = construct_tradematrix(input_wtf, input_isic)
+trademat = construct_tradematrix(input_wtf, input_isic)
 
-# new_trade_mat = aggregate_drop(trademat, drop_30[:, "new top 30"])
+new_trade_mat = aggregate_drop(trademat, drop_30[:, "new top 30"])
 
-# # grab the gross output data
-# gross_output_file = "./make-tradeshare-pricegap/UNIDO-data/UNIDO-grossoutput-data-2004.csv"
+# grab the gross output data
+gross_output_file = "./build-data-code/UNIDO-data/UNIDO-grossoutput-data-2004.csv"
 
-# gross_output = DataFrame(CSV.File(gross_output_file))
+gross_output = DataFrame(CSV.File(gross_output_file))
 
-# tradeshare, tradeshare_df = construct_tradeshare(new_trade_mat, gross_output)
+tradeshare, tradeshare_df = construct_tradeshare(new_trade_mat, gross_output)
 
-# df = build_dni(pricemat, gross_output, tradeshare)
+df = build_dni(pricemat, gross_output, tradeshare)
 
-# outfile = "pricegap-df-2004.csv"
+outfile = "pricegap-df-2004.csv"
 
-# CSV.write(outfile, df, header = true)  # Write the DataFrame to a CSV file
+CSV.write(outfile, df, header = true)  # Write the DataFrame to a CSV file
 
 
 
