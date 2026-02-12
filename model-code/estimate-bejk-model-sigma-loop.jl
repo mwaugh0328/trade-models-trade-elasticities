0844141206eac958efd91a92b11cdff2eef20734
Waugh <IRMEW01@red.minneapolisfed.org>
2026-02-11 18:15:07 -0600

    loops on melitz / bejk sigma

--- model-code/estimate-bejk-model-sigma-loop.jl
+++ model-code/estimate-bejk-model-sigma-loop.jl
@@ -14,16 +14,18 @@ using LinearAlgebra
 ################################################################
 
 year = ["2004", "2011", "2017"]
-#year = ["2017"]
-model = "melitz-model2"
+# year = ["2017"]
+model = "bejk"
+
 Nruns = 36
-Nboots = 2
-Ngoods = 50000
+Nboots = 100
+Ngoods = 100000
 
 dirname = "./data/"
 
 # Loop over different sigma values
 for σ in 1.5:0.5:3.0
+    
     println("Running estimation for σ = ", σ)
     
     # Create date string based on sigma value
@@ -33,19 +35,19 @@ for σ in 1.5:0.5:3.0
     ###############################################################################################################################
     method = "over"
     
-    
     dfout = estimate_all(year, σ, model, method, Nruns, Nboots, dirname; Ngoods = Ngoods)
     
     CSV.write("./results/"*model*"-estimate-"*method*"-"*date*".csv", dfout; writeheader = true)
     
-    ##############################################################################################################################
+    ###############################################################################################################################
     
     method = "exact"
-        
+    
     dfout = estimate_all(year, σ, model, method, Nruns, Nboots, dirname; Ngoods = Ngoods)
     
     CSV.write("./results/"*model*"-estimate-"*method*"-"*date*".csv", dfout; writeheader = true)
     
     println("Completed estimation for σ = ", σ)
     println("="^80)
+    
 end
